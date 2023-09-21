f8bcd59eef14348848a5d5cccb62f26af9b1b6fc
cheonYen <cyed312@gmail.com>
2023-09-21 17:54:31 +0900

    11번째 커밋

--- js/main.js
+++ js/main.js
@@ -15,3 +15,23 @@ skillList.forEach((skill) => {
     viewDesc.innerHTML = choiceDesc;
   });
 });
+
+let miniProjectLi = document.querySelectorAll('#miniProject .contentsWrap .projectList li a .imgWrap img');
+let isMobile = window.matchMedia("(max-width: 768px)");
+
+function updateImages() {
+  if(!isMobile.matches) {
+    for(let a = 0; a < miniProjectLi.length; a++) {
+      miniProjectLi[a].setAttribute('src', `img/mini0`+[a + 1]+`.png`);
+    }
+  } else{
+    for(let a = 0; a < miniProjectLi.length; a++) {
+      miniProjectLi[a].setAttribute('src', `img/arrowLeftToRightDiagonal.svg`);
+    }
+  }
+}
+// 페이지가 로드될 때 초기 이미지 업데이트
+window.onload = () => {
+  updateImages(); 
+}
+window.addEventListener('resize', updateImages); 
